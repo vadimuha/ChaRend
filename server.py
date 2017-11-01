42df1a3c549ed88a2cf6a810167851956476ad0c
vadimuha <vadimuha13@gmail.com>
2017-11-01 09:20:10 +0200

    Main template done

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "TODO"
+	return render_template("template/main.html")
