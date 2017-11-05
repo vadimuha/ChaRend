2982f5a1aa572296a72d5d18ca9ef72fa88ad38d
vadimuha <vadimuha13@gmail.com>
2017-11-05 17:14:59 +0200

    Basic register done, do register_cont

---
+++ Controller/db.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,request,jsonify,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:vadimuha150@localhost/charend"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+	__tablename__ = "users"
+	id = db.Column("id",db.Integer,primary_key=True)
+	username = db.Column("username",db.Unicode,unique=True)
+	passwd = db.Column("pass",db.Unicode)
+	status = db.Column("status",db.Integer)
+	name = db.Column("name",db.Unicode)
+	surname = db.Column("surname",db.Unicode)
+	about = db.Column("about",db.Unicode)
+	email = db.Column("email",db.Unicode)
+	day_of_birth = db.Column("day_of_birth",db.Unicode)
+	def __init__ (self,username,passwd,name,surname,about,email):
+		self.username = username
+		self.passwd = passwd
+		self.status = 0
+		self.name = name
+		self.surname = surname
+		self.about = about
+		self.email = email
+	def cont(self,name,surname,about,day):
+		pass
+	def jsn(self):
+		return {
+			"username": self.username
+		}
+
+class Groups(db.Model):
+	id = db.Column("id",db.Integer,primary_key=True)
+	name = db.Column("name",db.Unicode,unique=True)
+	def __init__(self,name):
+		self.name = name
--- server.py
+++ server.py
@@ -1,13 +1,47 @@
-from flask import Flask,render_template
+import sys
+
+sys.path.insert(0, 'Controller')
+from db import *
 
 app = Flask(__name__)
 
+id = 0
 @app.route("/")
 def index():
-	return render_template("registration.html")
+	return render_template("login.html")
 @app.route("/login",methods=['POST','GET'])
 def login():
 	return render_template("apology.html")
 @app.route("/register",methods=['POST','GET'])
 def register():
-	return render_template("apology.html")
+	if request.method == "POST":
+		username = request.form.get("username")
+		email = request.form.get("email")
+		password = request.form.get("pass")
+		re_password = request.form.get("re-pass")
+		if password != re_password:
+			return render_template("hack.html")
+		query = Users.query.order_by(Users.username).all()
+		users = [e.jsn() for e in query]
+		if username in users:
+			return render_template("hack.html")
+		new_ex = Users(username,password,None,None,None,email)
+		id = new_ex.username
+		db.session.add(new_ex)
+		db.session.commit()
+		return redirect(url_for('register_cont',username=username))
+	else:
+		return render_template("hack.html")
+@app.route('/register_cont',methods=['POST','GET'])
+def register_cont():
+	user = request.args['username']
+	query = Users.query.filter_by(username=user).first()
+	print(query.id)
+	return render_template("register_cont.html")
+
+''' Ajax section '''
+@app.route("/get_users",methods=['POST'])
+def get_users():
+	query = Users.query.order_by(Users.username).all()
+	return jsonify([e.jsn() for e in query])
+
